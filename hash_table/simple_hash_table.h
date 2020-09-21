630341296484baef6b4251f58c9de4747a2774db
chenxi65535 <chenxi65535@gmail.com>
2020-09-21 21:42:03 +0800

    init: upload simple_hash_table header file

---
+++ hash_table/simple_hash_table.h
@@ -0,0 +1,27 @@
+//
+// Created by Souris on 9/21/2020.
+//
+
+#ifndef DATA_STRUCTURE_SIMPLE_HASH_TABLE_H
+#define DATA_STRUCTURE_SIMPLE_HASH_TABLE_H
+
+struct node {
+    int key;
+    int val;
+    struct node *next;
+};
+
+struct table {
+    int size;
+    struct node **list;
+};
+
+struct table *create_table(int size);
+
+int hashcode(struct table *t, int key);
+
+void insert(struct table *t, int key, int val);
+
+int lookup(struct table *t, int key);
+
+#endif //DATA_STRUCTURE_SIMPLE_HASH_TABLE_H
